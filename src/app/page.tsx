4d314a349eba7d2ac40989ba06762c5366162652
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-12-28 17:09:46 -0300

    Update Corner component styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,10 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="bg-[#191919] h-screen w-screen">
-      <Corner size={200} padding={15} className='p-4'>
-        <h1 className='text-white text-2xl'>Bem Vindo</h1>
+      <Corner size={250} padding={20} className='p-12'>
+        <h1 className='text-white text-9xl font-thin p-8 pl-12'>Bem Vindo</h1>
+        <Corner size={250} padding={20} className='p-12' inverted />
       </Corner>
-    </main>
+    </main >
   )
 }
--- src/components/corner.tsx
+++ src/components/corner.tsx
@@ -11,9 +11,9 @@ interface CornerProps {
 export function Corner({ size, padding = 20, inverted, children, className }: CornerProps) {
   return (
     <div className={cn(className)}>
-      <div className={cn("from-blue-500 to-pink-400 bg-gradient-to-br", inverted ? "rounded-l-full" : "rounded-r-full")} style={{ width: size, height: padding, translate: `${inverted ? `0 ${size}px` : ""}` }} />
+      <div className={cn("from-blue-500 to-pink-400", inverted ? "rounded-l-full bg-gradient-to-l" : "rounded-r-full bg-gradient-to-r")} style={{ width: size, height: padding, translate: `${inverted ? `0 ${size}px` : ""}` }} />
       <div className="flex flex-row">
-        <div className={cn("from-blue-500 to-pink-400 bg-gradient-to-br", inverted ? "rounded-t-full" : "rounded-b-full")} style={{ height: size, width: padding, translate: `${inverted ? `${size}px 0` : ""}` }} />
+        <div className={cn("from-blue-500 to-pink-400", inverted ? "rounded-t-full bg-gradient-to-t" : "rounded-b-full bg-gradient-to-b")} style={{ height: size, width: padding, translate: `${inverted ? `${size}px 0` : ""}` }} />
         {children}
       </div>
     </div>
