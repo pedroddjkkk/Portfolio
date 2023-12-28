6b1b1f66070c056daf708a1a596bea5f0c189f3a
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-12-28 16:42:02 -0300

    Update Corner component size and add welcome message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="bg-[#191919] h-screen w-screen">
-      <Corner size={150} padding={15}/>
+      <Corner size={200} padding={15} className='p-4'>
+        <h1 className='text-white text-2xl'>Bem Vindo</h1>
+      </Corner>
     </main>
   )
 }
--- src/components/corner.tsx
+++ src/components/corner.tsx
@@ -4,13 +4,18 @@ interface CornerProps {
   size: number;
   padding?: number;
   inverted?: boolean;
+  children?: React.ReactNode;
+  className?: string;
 }
 
-export function Corner({ size, padding = 20, inverted }: CornerProps) {
+export function Corner({ size, padding = 20, inverted, children, className }: CornerProps) {
   return (
-    <>
-      <div className={cn(`from-green-500 to-blue-400 bg-gradient-to-br`, inverted ? "rounded-l-full" : "rounded-r-full")} style={{ width: size, height: padding, translate: `${inverted ? `0 ${size}px` : ""}` }} />
-      <div className={cn(`from-green-500 to-blue-400 bg-gradient-to-br`, inverted ? "rounded-t-full" : "rounded-b-full")} style={{ height: size, width: padding, translate: `${inverted ? `${size}px 0` : ""}` }} />
-    </>
+    <div className={cn(className)}>
+      <div className={cn("from-blue-500 to-pink-400 bg-gradient-to-br", inverted ? "rounded-l-full" : "rounded-r-full")} style={{ width: size, height: padding, translate: `${inverted ? `0 ${size}px` : ""}` }} />
+      <div className="flex flex-row">
+        <div className={cn("from-blue-500 to-pink-400 bg-gradient-to-br", inverted ? "rounded-t-full" : "rounded-b-full")} style={{ height: size, width: padding, translate: `${inverted ? `${size}px 0` : ""}` }} />
+        {children}
+      </div>
+    </div>
   )
 }
