58304acbb3412c5bfd3c689e84e253b4f1167022
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2023-12-28 16:28:25 -0300

    Update Corner component padding and add inverted option

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="bg-[#191919] h-screen w-screen">
-      <Corner size={150} padding={10} />
+      <Corner size={150} padding={15} i/>
     </main>
   )
 }
--- src/components/corner.tsx
+++ src/components/corner.tsx
@@ -3,13 +3,14 @@ import { cn } from "@/lib/cn";
 interface CornerProps {
   size: number;
   padding?: number;
+  inverted?: boolean;
 }
 
-export function Corner({ size, padding = 20 }: CornerProps) {
+export function Corner({ size, padding = 20, inverted }: CornerProps) {
   return (
     <>
-      <div className={`from-green-500 to-blue-400 bg-gradient-to-br rounded-r-full`} style={{ width: size, height: padding }} />
-      <div className={`from-green-500 to-blue-400 bg-gradient-to-br rounded-b-full`} style={{ height: size, width: padding }} />
+      <div className={cn(`from-green-500 to-blue-400 bg-gradient-to-br`, inverted ? "rounded-l-full" : "rounded-r-full")} style={{ width: size, height: padding, translate: `${inverted ? `0 ${size}px` : ""}` }} />
+      <div className={cn(`from-green-500 to-blue-400 bg-gradient-to-br`, inverted ? "rounded-t-full" : "rounded-b-full")} style={{ height: size, width: padding, translate: `${inverted ? `${size}px 0` : ""}` }} />
     </>
   )
 }
