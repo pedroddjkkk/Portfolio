3a0dec320a71ec2f12200bf732820bbc964bdbbc
Pedro Aparecido Valério <pedroddjkk@gmail.com>
2024-01-03 09:12:41 -0300

    Refactor page layout and styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ import { FaGithub, FaNpm } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="bg-[#191919] h-screen w-screen flex justify-center">
-      <div className='w-[90%] mt-32 max-w-[1530px]'>
+    <main className="bg-[#191919] h-fit md:py-0 md:h-screen py-10 flex justify-center pr-[18px]">
+      <div className='w-[90%] md:mt-32 mt-8 max-w-[1530px]'>
         <Corner size={250} padding={18} twoCorners className='flex flex-col bg-[#17191a]'>
           <div className='p-8 w-full flex justify-between items-center'>
             <Image src={"/logo_pv_4_gray_2.png"} alt='logo' width={150} height={200}></Image>
@@ -19,14 +19,14 @@ export default function Home() {
               <Link href={"https://github.com/pedroddjkkk"} target='_blank'>
                 <FaGithub className="text-gray-400 hover:text-gray-300 transition-colors duration-300" size={32} />
               </Link>
-              <Link href={"https://www.npmjs.com/package/ormize"} target='_blank'>
+              <Link href={"https://www.npmjs.com/~pedroddjkkk"} target='_blank'>
                 <FaNpm className="text-gray-400 hover:text-gray-300 transition-colors duration-300" size={32} />
               </Link>
             </div>
           </div>
-          <div className='flex p-8 pt-4'>
-            <section className='flex flex-col pl-12 w-[60%]'>
-              <h1 className='text-white text-8xl font-thin'>Bem Vindo</h1>
+          <div className='flex md:p-8 p-0 pt-4 flex-col md:flex-row items-center'>
+            <section className='flex flex-col md:pl-12 p-2 md:w-[60%] w-full'>
+              <h1 className='text-white text-8xl font-thin text-center md:text-start'>Bem Vindo</h1>
               <span className='text-white font-thin text-lg pl-3 mt-6'>
                 Meu nome é Pedro Valério. Sou desenvolvedor full-stack, apaixonado por tecnologia e por resolver problemas.
               </span>
@@ -36,7 +36,7 @@ export default function Home() {
                 Dentre as aplicações que desenvolvi, destaco o ecommerce de uma grande empresa de cosmeticos.
               </span>
             </section>
-            <section className='ml-20 mt-6'>
+            <section className='md:ml-20 mt-6'>
               <h1 className='text-white text-3xl font-extralight mb-4'>Projetos</h1>
               <Corner size={60} padding={4} className='m-6 flex flex-col gap-4'>
                 <Project href='https://www.npmjs.com/package/ormize'>ormize</Project>
